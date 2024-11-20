38b9f7c9fb422c49c22555d0cfd2d00fe5122aa8
Your Name <you@example.com>
2024-11-20 10:31:07 +0100

    aus ts

--- Tuttas/main.tf
+++ Tuttas/main.tf
@@ -19,13 +19,13 @@ provider "cloudstack" {
 }
 
 # Netzwerk definieren
-resource "cloudstack_network" "vlan_network" {
-  name             = "VLAN-Network"
-  display_text     = "VLAN Network for Linux VMs"
-  network_offering = "a1d37c0a-c84b-44b3-99d2-92c872b9e11a"  # Beispiel-Network Offering
-  zone             = "a4848bf1-b2d1-4b39-97e3-72106df81f09" # Zone-ID
-  cidr             = "192.168.1.0/24"
-}
+#resource "cloudstack_network" "vlan_network" {
+#  name             = "VLAN-Network"
+#  display_text     = "VLAN Network for Linux VMs"
+#  network_offering = "551f5a5f-d5b6-459e-96c4-661cf43c68dc"  # Beispiel-Network Offering
+#  zone             = "a4848bf1-b2d1-4b39-97e3-72106df81f09" # Zone-ID
+#  cidr             = "192.168.1.0/24"
+#}
 
 # Virtuelle Maschine 1 erstellen
 resource "cloudstack_instance" "vm1" {
@@ -34,9 +34,9 @@ resource "cloudstack_instance" "vm1" {
   service_offering  = "Big Instance" # Ersetze mit dem passenden Service Offering
   template          = "5bfa057a-91dd-11ef-bd59-46e70d67c9bd" # Ersetze mit deiner Template-ID
   zone              = "a4848bf1-b2d1-4b39-97e3-72106df81f09" # Zone-ID
-  network_id        = cloudstack_network.vlan_network.id
-  expunge           = true # Automatisches Löschen bei Terraform Destroy
-
+  #network_id        = cloudstack_network.vlan_network.id
+  # expunge           = true # Automatisches Löschen bei Terraform Destroy
+  root_password     = "SecurePassword123!" # Hier wird das Root-Passwort gesetzt
   # Root-Disk konfigurieren
   root_disk_size    = 20 # Größe der Root-Disk in GB
 }
@@ -48,17 +48,17 @@ resource "cloudstack_instance" "vm2" {
   service_offering  = "Big Instance" # Ersetze mit dem passenden Service Offering
   template          = "5bfa057a-91dd-11ef-bd59-46e70d67c9bd" # Ersetze mit deiner Template-ID
   zone              = "a4848bf1-b2d1-4b39-97e3-72106df81f09" # Zone-ID
-  network_id        = cloudstack_network.vlan_network.id
-  expunge           = true # Automatisches Löschen bei Terraform Destroy
-
+  #network_id        = cloudstack_network.vlan_network.id
+  # expunge           = true # Automatisches Löschen bei Terraform Destroy
+  root_password     = "SecurePassword123!" # Hier wird das Root-Passwort gesetzt
   # Root-Disk konfigurieren
   root_disk_size    = 20 # Größe der Root-Disk in GB
 }
 
 # Ausgaben definieren
-output "network_id" {
-  value = cloudstack_network.vlan_network.id
-}
+#output "network_id" {
+#  value = cloudstack_network.vlan_network.id
+#}
 
 output "vm1_id" {
   value = cloudstack_instance.vm1.id
