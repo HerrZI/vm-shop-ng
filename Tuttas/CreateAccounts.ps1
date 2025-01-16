a31ad7f39ac4f21ce84daff9d4d7b1c72da2b930
jtuttas <jtuttas@gmx.net>
2025-01-16 18:18:39 +0100

    Mit Accounts anlegen

--- Tuttas/CreateAccounts.ps1
+++ Tuttas/CreateAccounts.ps1
@@ -1,28 +1,74 @@
-﻿### PowerShell Script to Retrieve All Users from an Azure AD Group
-
-# Connect to Azure AD
-Connect-AzureAD
-
-# Specify the Azure AD group name or object ID
-$GroupName = "FIAE24J"
+﻿$data = Import-Excel "d:\Temp\vm-shop-ng\Tuttas\Wahl.xlsx"
 $domainID = "5b7e8018-d8bf-4e60-9f15-8d6083dbbfcb"
-# Retrieve the group object
-$Group = Get-AzureADGroup -Filter "DisplayName eq '$GroupName'"
+$groupPassword = "grouppassword"
+$userPassword = "geheim"
 
-if (!$Group) {
-    Write-Output "Group not found!"
-    exit
+
+# Funktion, um Benutzername aus der E-Mail-Adresse zu generieren
+function Get-UsernameFromEmail {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$Email
+    )
+    $Email = $Email -replace '\.', '_'
+    return $Email.Split("@")[0]
 }
 
-# Retrieve group members
-$Members = Get-AzureADGroupMember -ObjectId $Group[0].ObjectId -All $true
+# Liste der bereits erstellten Accounts (Gruppen)
+$createdAccounts = @{}
 
-# Display members
-if ($Members) {
-    $Members | Select-Object GivenName, Surname, UserPrincipalName | Format-Table -AutoSize
-} else {
-    Write-Output "No members found in the group."
+# Durchlaufe die Daten und erstelle Accounts und Benutzer
+foreach ($row in $data) {
+    if ($row.Gruppe -eq $null) {
+        continue
+    }
+
+    # Werte aus der Zeile
+    $group = $row.Gruppe -replace '\s', ''
+    $email = $row.'E-Mail-Adresse'
+    $firstname = $row.Vorname  -replace ' ', '_'
+    $lastname = $row.Nachname  -replace ' ', '_'
+    $idNumber = $row.'ID-Nummer'
+    $username = Get-UsernameFromEmail -Email $email
+    $username = $username  -replace ' ', '_'
+
+    
+
+    # Prüfe, ob der Account für die Gruppe existiert
+    if (-not $createdAccounts.ContainsKey($group)) {
+        Write-Host "Erstelle Account für Gruppe: $group"
+
+        # Account erstellen
+        try {
+            
+            $createAccountResult = New-CloudStackAccount -AccountName $group -DomainID "$($domainID)" -AccountType "0" -Email "$($group)@mm-bbs.de" -FirstName "Group" -LastName $group -UserName $group -Password $groupPassword
+            if ($createAccountResult) {
+                $createdAccounts[$group] = $createAccountResult.ID
+                Write-Host "Account '$group' erfolgreich erstellt."
+            } else {
+                Write-Warning "Account '$group' konnte nicht erstellt werden."
+            }
+        } catch {
+            Write-Error "Fehler beim Erstellen des Accounts '$group'"
+        }
+
+    }
+
+    # Benutzer dem entsprechenden Account hinzufügen
+    $accountID = $createdAccounts[$group]
+    Write-Host "Füge Benutzer '$username' zur Gruppe '$group' hinzu."
+
+    try {
+        #Write-Host "New CloudSatck User username=$username password=$userPassword Firstname=$firstname LastName=$lastname -Email $email -Accountname=$group -DomainID $domainID"
+        $newUserResult = New-CloudStackUser -Username $username -Password $userPassword -FirstName $firstname -LastName $lastname  -Email $email -AccountName $group -DomainID "$domainID"
+
+        if ($newUserResult) {
+            Write-Host "Benutzer '$username' erfolgreich hinzugefügt."
+        } else {
+            Write-Warning "Benutzer '$username' konnte nicht hinzugefügt werden."
+        }
+    } catch {
+        Write-Error "Fehler beim Hinzufügen von Benutzer '$username': $_"
+    }
+    
 }
-
-$Members | Select-Object GivenName, Surname, UserPrincipalName | ForEach-Object {New-CloudStackAccount -AccountName $_.Surname.Replace(" ","_") -Email $_.UserPrincipalName -FirstName "$($_.GivenName.Replace(" ","_"))" -LastName $_.Surname -Username $_.Surname.Replace(" ","_") -Password "geheim" -DomainID $domainID} 
-#Write-Output "Script completed!"
