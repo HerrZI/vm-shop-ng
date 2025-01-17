3043c783f64119fd5eb2ee081b5658c068d3e837
jtuttas <jtuttas@gmx.net>
2025-01-17 07:50:14 +0100

    schöner

--- Tuttas/CreateAccounts.ps1
+++ Tuttas/CreateAccounts.ps1
@@ -1,7 +1,41 @@
-﻿$data = Import-Excel "d:\Temp\vm-shop-ng\Tuttas\Wahl.xlsx"
-$domainID = "5b7e8018-d8bf-4e60-9f15-8d6083dbbfcb"
-$groupPassword = "grouppassword"
-$userPassword = "geheim"
+﻿if (-not (Get-Module -ListAvailable -Name ImportExcel)) {
+    Install-Module -Name ImportExcel -Force -Scope CurrentUser
+}
+if (-not (Get-Module -ListAvailable -Name CSHelper)) {
+    Install-Module -Name CSHelper -Force -Scope CurrentUser
+}
+
+if (-not $Global:CloudStackBaseUrl -or -not $Global:CloudStackApiKey -or -not $Global:CloudStackSecretKey) {
+    $ApiKey = Read-Host "Bitte geben Sie den CloudStack API-Key ein: "
+    $Secret = Read-Host "Bitte geben Sie den CloudStack Secret-Key ein: "
+    $url = "https://cloudstack.mm-bbs.de/client/api"
+    Connect-CloudStack -BaseUrl $url -ApiKey $ApiKey -SecretKey $Secret        
+    if (-not $Global:CloudStackBaseUrl -or -not $Global:CloudStackApiKey -or -not $Global:CloudStackSecretKey) {
+        Write-Error "Fehler beim Verbinden mit CloudStack."
+        exit
+    }
+}
+
+
+# Abfrage des Pfades mit variable als Default-Wert
+$pfad = Read-Host "Bitte geben Sie den Pfad zur Excel-Datei ein [Bsp.: d:\Temp\vm-shop-ng\Tuttas\Wahl.xlsx]: "
+if ($pfad -eq "") {
+    $pfad = "d:\Temp\vm-shop-ng\Tuttas\Wahl.xlsx"
+}
+if (-not (Test-Path $pfad)) {
+    Write-Error "Datei '$pfad' existiert nicht."
+    exit
+} 
+$data = Import-Excel $pfad
+
+
+$domainID = Read-Host "Bitte geben Sie die Domain-ID ein [z.B. 5b7e8018-d8bf-4e60-9f15-8d6083dbbfcb]: "
+if ($domainID -eq "") {
+    $domainID = "5b7e8018-d8bf-4e60-9f15-8d6083dbbfcb"
+}
+
+$groupPassword = "grouppassword" # Passwort für den Account (die Gruppe)
+$userPassword = "geheim" # Passwort für den User
 
 
 # Funktion, um Benutzername aus der E-Mail-Adresse zu generieren
@@ -10,12 +44,12 @@ function Get-UsernameFromEmail {
         [Parameter(Mandatory = $true)]
         [string]$Email
     )
-    $Email = $Email -replace '\.', '_'
     return $Email.Split("@")[0]
 }
 
 # Liste der bereits erstellten Accounts (Gruppen)
 $createdAccounts = @{}
+Get-CloudStackAccounts -DomainID $domainID | ForEach-Object {$createdAccounts[$_.Name]=$_}
 
 # Durchlaufe die Daten und erstelle Accounts und Benutzer
 foreach ($row in $data) {
@@ -53,22 +87,35 @@ foreach ($row in $data) {
         }
 
     }
+    else {
+        Write-Warning "Account für Gruppe: $group existiert bereits"
+    }
 
     # Benutzer dem entsprechenden Account hinzufügen
     $accountID = $createdAccounts[$group]
-    Write-Host "Füge Benutzer '$username' zur Gruppe '$group' hinzu."
 
-    try {
-        #Write-Host "New CloudSatck User username=$username password=$userPassword Firstname=$firstname LastName=$lastname -Email $email -Accountname=$group -DomainID $domainID"
-        $newUserResult = New-CloudStackUser -Username $username -Password $userPassword -FirstName $firstname -LastName $lastname  -Email $email -AccountName $group -DomainID "$domainID"
+    $userList = @{}
+    Get-CloudStackUsers -AccountName $group -DomainID $domainID | ForEach-Object {$userList[$_.Username]=$_}
 
-        if ($newUserResult) {
-            Write-Host "Benutzer '$username' erfolgreich hinzugefügt."
-        } else {
-            Write-Warning "Benutzer '$username' konnte nicht hinzugefügt werden."
+
+    if (-not $userList.ContainsKey($username)) {
+        Write-Host "Füge Benutzer '$username' zur Gruppe '$group' hinzu."
+
+        try {
+            #Write-Host "New CloudSatck User username=$username password=$userPassword Firstname=$firstname LastName=$lastname -Email $email -Accountname=$group -DomainID $domainID"
+            $newUserResult = New-CloudStackUser -Username $username -Password $userPassword -FirstName $firstname -LastName $lastname  -Email $email -AccountName $group -DomainID "$domainID"
+
+            if ($newUserResult) {
+                Write-Host "Benutzer '$username' erfolgreich hinzugefügt."
+            } else {
+                Write-Warning "Benutzer '$username' konnte nicht hinzugefügt werden."
+            }
+        } catch {
+            #Write-Error "Fehler beim Hinzufügen von Benutzer '$username': $_"
         }
-    } catch {
-        Write-Error "Fehler beim Hinzufügen von Benutzer '$username': $_"
+    }
+    else {
+        Write-Warning "Benutzer $username existiert bereits in der Gruppe $group"
     }
     
 }
