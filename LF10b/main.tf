c2ec5970210fea9bb2a7b8a09490b5e7d8e56dab
HerrZI <ziegner@mmbbs.de>
2025-02-24 13:32:41 +0100

    main.tf hinzugefügt

---
+++ LF10b/main.tf
@@ -0,0 +1,18 @@
+variable "api_url" {}
+variable "api_key" {}
+variable "secret_key" {}
+
+terraform {
+  required_providers {
+    cloudstack = {
+      source  = "cloudstack/cloudstack"
+      version = "0.5.0"
+    }
+  }
+}
+
+provider "cloudstack" {
+  api_url    = var.api_url
+  api_key    = var.api_key
+  secret_key = var.secret_key
+}
